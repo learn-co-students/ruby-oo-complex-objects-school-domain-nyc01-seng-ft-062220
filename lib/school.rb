c116ba3f4c0d7186e1b30400f7c7f4d39100a3e8
Aiannn <soktoevayan@gmail.com>
2020-06-23 18:07:32 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,29 @@
 # code here!
+class School
+    def initialize(name, roster={})
+        @name = name
+        @roster = roster
+    end    
+
+    def roster
+        @roster
+    end
+
+    def add_student(st_name, grade)
+        if !@roster[grade]
+            @roster[grade] = []
+        end    
+        @roster[grade].push(st_name) 
+    end     
+
+    def grade(grade)
+        return @roster[grade]
+    end     
+
+    def sort
+        roster.each do |key, value|
+            value.sort 
+        end
+        roster  
+    end             
+end    
