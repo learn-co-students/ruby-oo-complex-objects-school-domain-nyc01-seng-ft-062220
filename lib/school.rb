261bb1a4c03d30424b5a966b5b66c88d69b59a79
catd825 <catd825@gmail.com>
2020-06-23 19:20:37 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
 # code here!
+require 'pry'
+
+class School
+
+    def initialize(roster)
+        @roster={}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student(student, grade)
+        if !roster.has_key?(grade)
+          @roster[grade] = []
+          @roster[grade] << student
+        else
+            @roster[grade] << student 
+        end
+    end
+
+    def grade(grade)
+        @roster[grade].sort
+    end
+
+    def sort
+        @roster.map do |key,value|
+          @roster[key] = value.sort
+        end
+        @roster
+     end
+
+end
+
+# [2] pry(#<School>)> @roster
+# => {9=>["Homer Simpson"],
+#  10=>["Avi Flombaum", "Jeff Baird"],
+#  7=>["Blake Johnson"]}
+# [3] pry(#<School>)> grade(10)
