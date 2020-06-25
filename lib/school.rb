9f009c61af750d2cbc14fadefd8e91ad6f99dbf0
“jzhouerm” <jzhouerm@hotmail.comgit config --global user.name jzhouermgit config --global user.email jzhouerm@hotmail.com>
2020-06-24 23:06:43 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+    def initialize(student)
+        @student = student
+        @roster = {}
+    end
+
+    def roster
+        @roster
+    end
+
+    def add_student (student, grade)
+        @roster[grade] ||= []
+        # if self.roster[grade] already exists, leave it alone!
+        #else, set self.roster[grade] = []
+        @roster[grade] << student
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.each do |grade, student|
+            student.sort!
+        end
+    end
+
+
+end
